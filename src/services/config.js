186ba462e65733d66b90a610b717e75aeff357f9
marianozamorano <mjzamorano84@gmail.com>
2024-01-24 00:22:44 -0300

    Entrega Final

--- src/services/config.js
+++ src/services/config.js
@@ -2,14 +2,13 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyDc9UG6v0cDfIVwSQjCmgBmhybtzR-eXxM",
+    apiKey: import.meta.env.VITE_API_KEY_FIREBASE_CONFIG,
     authDomain: "marolio-react-8510b.firebaseapp.com",
     projectId: "marolio-react-8510b",
     storageBucket: "marolio-react-8510b.appspot.com",
     messagingSenderId: "177517771926",
     appId: "1:177517771926:web:f95c0d926e9923016d96b7"
 };
-
 
 const app = initializeApp(firebaseConfig);
 
